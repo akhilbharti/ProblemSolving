b0b60bf2fee21d18e874d068b33773ea76986b04
Akhil Bharti <akhbharti@gmail.com>
2020-05-10 01:00:36 +0530

    loop from 1 to floor(sqrt(n))

---
+++ perfectSquare.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+for(var i=1;i*i<=num;i++){
+    if((num%i===0) &&(num/i===i))
+        return true
+}
+    return false
+};
